e0fb9b7f090a9740789499990a62ab89ffca45f4
Gina Häußge <gina@octoprint.org>
2017-11-24 17:13:28 +0100

    Increment version & use octoprint_setuptools

--- setup.py
+++ setup.py
@@ -1,42 +1,99 @@
 # coding=utf-8
-import setuptools
 
-def package_data_dirs(source, sub_folders):
-	import os
-	dirs = []
+########################################################################################################################
+### Do not forget to adjust the following variables to your own plugin.
 
-	for d in sub_folders:
-		for dirname, _, files in os.walk(os.path.join(source, d)):
-			dirname = os.path.relpath(dirname, source)
-			for f in files:
-				dirs.append(os.path.join(dirname, f))
+# The plugin's identifier, has to be unique
+plugin_identifier = "growl"
 
-	return dirs
+# The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
+plugin_package = "octoprint_growl"
 
-def params():
-	name = "OctoPrint-Growl"
-	version = "0.1.0"
+# The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
+# plugin module
+plugin_name = "OctoPrint-Growl"
 
-	description = "Get Growl/GNTP notifications from OctoPrint"
-	long_description = "TODO"
-	author = "Gina Häußge"
-	author_email = "osd@foosel.net"
-	url = "http://github.com/OctoPrint/OctoPrint-Growl"
-	license = "AGPLv3"
+# The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
+plugin_version = "0.1.1"
 
-	packages = ["octoprint_growl"]
-	package_data = {"octoprint_growl": package_data_dirs('octoprint_growl', ['static', 'templates'])}
+# The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
+# module
+plugin_description = """Get Growl/GNTP notifications from OctoPrint"""
 
-	include_package_data = True
-	zip_safe = False
-	install_requires = open("requirements.txt").read().split("\n")
+# The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
+plugin_author = "Gina Häußge"
 
-	entry_points = {
-		"octoprint.plugin": [
-			"growl = octoprint_growl"
-		]
-	}
+# The plugin's author's mail address.
+plugin_author_email = "gina@octoprint.org"
 
-	return locals()
+# The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
+plugin_url = "https://github.com/OctoPrint/OctoPrint-Growl"
 
-setuptools.setup(**params())
+# The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
+plugin_license = "AGPLv3"
+
+# Any additional requirements besides OctoPrint should be listed here
+plugin_requires = ["gntp>=1.0.3,<2"]
+
+### --------------------------------------------------------------------------------------------------------------------
+### More advanced options that you usually shouldn't have to touch follow after this point
+### --------------------------------------------------------------------------------------------------------------------
+
+# Additional package data to install for this plugin. The subfolders "templates", "static" and "translations" will
+# already be installed automatically if they exist. Note that if you add something here you'll also need to update
+# MANIFEST.in to match to ensure that python setup.py sdist produces a source distribution that contains all your
+# files. This is sadly due to how python's setup.py works, see also http://stackoverflow.com/a/14159430/2028598
+plugin_additional_data = []
+
+# Any additional python packages you need to install with your plugin that are not contained in <plugin_package>.*
+plugin_additional_packages = []
+
+# Any python packages within <plugin_package>.* you do NOT want to install with your plugin
+plugin_ignored_packages = []
+
+# Additional parameters for the call to setuptools.setup. If your plugin wants to register additional entry points,
+# define dependency links or other things like that, this is the place to go. Will be merged recursively with the
+# default setup parameters as provided by octoprint_setuptools.create_plugin_setup_parameters using
+# octoprint.util.dict_merge.
+#
+# Example:
+#     plugin_requires = ["someDependency==dev"]
+#     additional_setup_parameters = {"dependency_links": ["https://github.com/someUser/someRepo/archive/master.zip#egg=someDependency-dev"]}
+additional_setup_parameters = {}
+
+########################################################################################################################
+
+from setuptools import setup
+
+try:
+	import octoprint_setuptools
+except:
+	print("Could not import OctoPrint's setuptools, are you sure you are running that under "
+	      "the same python installation that OctoPrint is installed under?")
+	import sys
+	sys.exit(-1)
+
+setup_parameters = octoprint_setuptools.create_plugin_setup_parameters(
+	identifier=plugin_identifier,
+	package=plugin_package,
+	name=plugin_name,
+	version=plugin_version,
+	description=plugin_description,
+	author=plugin_author,
+	mail=plugin_author_email,
+	url=plugin_url,
+	license=plugin_license,
+	requires=plugin_requires,
+	additional_packages=plugin_additional_packages,
+	ignored_packages=plugin_ignored_packages,
+	additional_data=plugin_additional_data
+)
+
+if len(additional_setup_parameters):
+	from octoprint.util import dict_merge
+	setup_parameters = dict_merge(setup_parameters, additional_setup_parameters)
+
+setup(**setup_parameters)
+
+
+
